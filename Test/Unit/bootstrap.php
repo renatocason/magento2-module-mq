54ac487edeacde52534eb8bda930e68153bcb444
Renato Cason <renato.cason@gmail.com>
2018-12-05 13:14:24 +0000

    Avoid multiple declaration of translation function

--- Test/Unit/bootstrap.php
+++ Test/Unit/bootstrap.php
@@ -3,20 +3,22 @@
 // @codingStandardsIgnoreFile
 require_once realpath(__DIR__ . '/../../vendor/autoload.php');
 
-/**
- * Create value-object \Magento\Framework\Phrase
- * 
- * @SuppressWarnings(PHPMD.ShortMethodName)
- * @return \Magento\Framework\Phrase
- */
-function __()
-{
-    $argc = func_get_args();
-
-    $text = array_shift($argc);
-    if (!empty($argc) && is_array($argc[0])) {
-        $argc = $argc[0];
+if (!function_exists('__')) {
+    /**
+     * Create value-object \Magento\Framework\Phrase
+     * 
+     * @SuppressWarnings(PHPMD.ShortMethodName)
+     * @return \Magento\Framework\Phrase
+     */
+    function __()
+    {
+        $argc = func_get_args();
+    
+        $text = array_shift($argc);
+        if (!empty($argc) && is_array($argc[0])) {
+            $argc = $argc[0];
+        }
+    
+        return new \Magento\Framework\Phrase($text, $argc);
     }
-
-    return new \Magento\Framework\Phrase($text, $argc);
 }
